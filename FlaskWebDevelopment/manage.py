783dfe0d3e0c0da4f9f3983b37e42864cbc0792b
dodo99 <tangtony@hotmail.com>
2016-11-05 20:12:28 -0400

    finished chapter 6

--- FlaskWebDevelopment/manage.py
+++ FlaskWebDevelopment/manage.py
@@ -7,12 +7,14 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-#from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
+import os
+from threading import Thread
 
 
 app = Flask(__name__)
@@ -20,11 +22,25 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:3622@localhost:5432/flaskwebdev'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+#app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+#app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_USERNAME'] = 'tonyltang'
+app.config['MAIL_PASSWORD'] = 'tt4853622'
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+#app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+app.config['FLASKY_ADMIN'] = 'tonyltang'
+
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
@@ -48,6 +64,21 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender = app.config['FLASKY_MAIL_SENDER'],
+                  recipients= [to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target = send_async_email, args = [app, msg])
+    thr.start()
+    return thr
 
 
 class NameForm(Form):
@@ -81,6 +112,11 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'],
+                           'New User',
+                           'mail/new_user',
+                           user = user)
         else:
             session['known']= True
 
@@ -107,5 +143,5 @@ if __name__ == '__main__':
     # this will run at http://localhost:5000
     #app.run(debug=True)
 
-    #run from the virtual environment as .\env\Scripts\python.exe app.py runserver --host 127.0.0.1
+    #run from the virtual environment as .\env\Scripts\python.exe manage.py runserver --host 127.0.0.1
     manager.run()
