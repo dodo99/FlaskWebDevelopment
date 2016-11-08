19deb5dde1aef9ffe4bc4de329c2d5550de9243e
dodo99 <tangtony@hotmail.com>
2016-11-08 00:17:42 -0500

    finished the  user/admin profile editing in chap 10

--- FlaskWebDevelopment/app/decorators.py
+++ FlaskWebDevelopment/app/decorators.py
@@ -3,11 +3,11 @@
 from functools import wraps
 from flask import abort
 from flask_login import current_user
-from .models import Persission
+from .models import Permission
 
 def permission_required(permission):
     def decorator(f):
-        @wraps
+        @wraps(f)
         def decorated_function( *args, **kwargs):
             if not current_user.can(permission):
                 abort(403)
