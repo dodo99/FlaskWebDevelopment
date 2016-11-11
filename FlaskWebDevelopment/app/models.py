88482a8975f54aa6616468f688d5664ca5f55642
dodo99 <tangtony@hotmail.com>
2016-11-11 01:41:23 -0500

    chapter 12, all done.

--- FlaskWebDevelopment/app/models.py
+++ FlaskWebDevelopment/app/models.py
@@ -81,6 +81,15 @@ class User(UserMixin, db.Model):
                                cascade = 'all, delete-orphan')
     
 
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
+
+
     def __init__(self, **kwargs):
         super( User, self).__init__(**kwargs)
         if self.role is None:
@@ -90,6 +99,7 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(default = True).first()
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        self.followed.append(Follow(followed = self))
 
 
     @property
@@ -99,6 +109,12 @@ class User(UserMixin, db.Model):
     @password.setter
     def password(self, password):
         self.password_hash = generate_password_hash(password)
+
+    @property
+    def followed_posts(self):
+        return Post.query.join(Follow, Follow.followed_id == Post.author_id).filter(Follow.follower_id == self.id)
+        #alternative query (same as above) db.session.query(Post).select_from(Follow).filter_by(follower_id=self.id).join(Post, Follow.followed_id == Post.author_id)  
+
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
